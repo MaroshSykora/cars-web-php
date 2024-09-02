efd7d9a7d63d8edc73df51da1c09e7c9523ff998
Marosh Sykora <sykoramaros@gmail.com>
2024-09-02 22:44:44 +0200

    soubor add.php opraven

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 // vypis aut
-include('DbConnect.php');
 require_once('Cars.php');
+include('DbConnect.php');
 
 // zavolani metody connect pro pripojeni do db (databaze)
 $conn = new DbConnect();
@@ -19,7 +19,7 @@ if (isset($_POST['add'])) {
   $instanceCars->addCar($brand, $model, $reg, $km, $year);
   header("Location: index.php");
   exit();
-
+}
 ?>
 
 <!-- HTML -->
@@ -34,20 +34,39 @@ if (isset($_POST['add'])) {
 </head>
 
 <body>
-  <section>
-    <div class="container">
-      <h2>Pridani auta</h2>
-
-      <form action="add.php" method="post">
-        <input type="text" name="brand" value="" class="form-control my-2" placeholder="znacka vyrobce" required>
-        <input type="text" name="model" value="" class="form-control my-2" placeholder="model vozidla" required>
-        <input type="text" name="reg" value="" class="form-control my-2" placeholder="registracni znacka" required>
-        <input type="number" name="km" value="" class="form-control my-2" placeholder="pocet najetych km" required>
-        <input type="number" name="year" value="" class="form-control my-2" placeholder="rok vyroby" required>
-        <input type="submit" value="Vloz auto" class="btn btn-primary my-2" name="add">
-      </form>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Auta</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Seznam aut</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="edit.php">Uprav auto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="add.php">Přidej auto</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </section>
+  </nav>
+  <div class="container">
+    <h2 class="h2">Přidání nového auta</h2>
+    <form action="add.php" method="post">
+      <input type="hidden" name="id" value="">
+      <input class="form-control my-2" name="brand" type="text" value="" placeholder="Zadejte značku" required />
+      <input class="form-control my-2" name="model" type="text" value="" placeholder="Zadejte model" required />
+      <input class="form-control my-2" name="reg" type="text" value="" placeholder="Zadejte registraci" required />
+      <input class="form-control my-2" name="km" type="number" value="" placeholder="Zadejte kilometry" required />
+      <input class="form-control my-2" name="year" type="number" value="" placeholder="Zadejte rok" required />
+      <input class="btn btn-primary my-2" type="submit" name="add" value="Vlož auto" />
+    </form>
+  </div>
 
 
   <!-- SKRIPT BOOTSTRAP -->
