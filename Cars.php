a70f462494df4ec67a3aadedfe18c73f039c3289
Marosh Sykora <sykoramaros@gmail.com>
2024-09-02 22:53:50 +0200

    soubor Cars.php opraven

--- Cars.php
+++ Cars.php
@@ -13,9 +13,9 @@ class Cars
 {
   private $dbConn;
   // predani konstruktoru jako parametr pripojeni do db
-  public function __construct($p_dbConn)
+  public function __construct($dbConn)
   {
-    $this->dbConn = $p_dbConn;
+    $this->dbConn = $dbConn;
   }
   // metoda, ktera vrati z db vsechna auta (bez parametru)
   // zaslani dotazu do databaze
@@ -29,7 +29,7 @@ class Cars
 
 
   // metoda, hledajici auta na zaklade zadanych parametru
-  public function filterCars($p_brand, $p_model, $p_reg)
+  public function filterCars($brand, $model, $reg)
   {
     // pravdivostni podminka 1=1 (zdelsena verze WHERE brand LIKE '"%" . $p_brand . "%"') zjistovani jestli je pri vyhledavani neco jako jako treba "skoda" nebo "Skoda"
     $sql = 'SELECT * FROM cars WHERE 1=1';
@@ -38,17 +38,17 @@ class Cars
 
     //  pridani podminek do dotazu podle parametru
     // ochrana proti sequal injection
-    if (!empty($p_brand)) {
+    if (!empty($brand)) {
       $sql .= " AND brand LIKE :brand";
-      $params[':brand'] = '%' . $p_brand . '%';
+      $params[':brand'] = '%' . $brand . '%';
     }
-    if (!empty($p_model)) {
+    if (!empty($model)) {
       $sql .= " AND model LIKE :model";
-      $params[':model'] = '%' . $p_model . '%';
+      $params[':model'] = '%' . $model . '%';
     }
     if (!empty($p_brand)) {
       $sql .= " AND reg LIKE :reg";
-      $params[':reg'] = '%' . $p_reg . '%';
+      $params[':reg'] = '%' . $reg . '%';
     }
 
     //  priprava SQL dotazu
@@ -60,53 +60,55 @@ class Cars
       $stmt->bindValue($param, $value, PDO::PARAM_STR);
       // vysledky
     }
+    // Vykonání SQL dotazu
     $stmt->execute();
-    // vlozeni do asociativniho pole
+    // Návrat výsledků jako pole asociativních polí
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
   // mazani auta
-  public function deleteCar($p_id)
+  public function deleteCar($id)
   {
     $sql = 'DELETE FROM cars WHERE id = :id';
     $stmt = $this->dbConn->prepare($sql);
-    $stmt->bindParam(':id', $p_id, PDO::PARAM_INT);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     return $stmt->execute();
   }
 
   // vraceni konkretniho auta
-  public function getCar($p_id)
+  public function getCar($id)
   {
     $sql = 'SELECT * FROM cars WHERE id = :id';
     $stmt = $this->dbConn->prepare($sql);
-    $stmt->bindParam(':id', $p_id, PDO::PARAM_INT);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
     return $stmt->fetch(PDO::FETCH_ASSOC);
   }
 
   // aktualizace existujiciho auta
-  public function updateCar($p_id, $p_brand, $p_model, $p_reg, $p_km, $p_year)
+  public function updateCar($id, $brand, $model, $reg, $km, $year)
   {
-    $sql = 'UPDATE cars SET brand = :brand, model =:model, reg = :reg, km =:km, year = :year WHERE id = :id';
+    $sql = "UPDATE cars SET brand = :brand, model =:model, reg = :reg, km =:km, year = :year WHERE id = :id";
     $stmt = $this->dbConn->prepare($sql);
-    $stmt->bindParam(':id', $p_id, PDO::PARAM_INT);
-    $stmt->bindParam(':brand', $p_brand, PDO::PARAM_STR);
-    $stmt->bindParam(':model', $p_model, PDO::PARAM_STR);
-    $stmt->bindParam(':reg', $p_reg, PDO::PARAM_STR);
-    $stmt->bindParam(':km', $p_km, PDO::PARAM_INT);
-    $stmt->bindParam(':year', $p_year, PDO::PARAM_INT);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+    $stmt->bindParam(':model', $model, PDO::PARAM_STR);
+    $stmt->bindParam(':reg', $reg, PDO::PARAM_STR);
+    $stmt->bindParam(':km', $km, PDO::PARAM_INT);
+    $stmt->bindParam(':year', $year, PDO::PARAM_INT);
     return $stmt->execute();
   }
 
   // pridani auta
-  public function addCar($p_brand, $p_model, $p_reg, $p_km, $p_year)
+  public function addCar($brand, $model, $reg, $km, $year)
   {
-    $sql = 'INSERT INTO cars (brand,model,reg,km, year) VALUES (:brand, :model, :reg, :km, :year)';
+    $sql = "INSERT INTO cars (brand,model,reg,km, year) VALUES (:brand, :model, :reg, :km, :year)";
     $stmt = $this->dbConn->prepare($sql);
-    $stmt->bindParam(':brand', $p_brand, PDO::PARAM_STR);
-    $stmt->bindParam(':model', $p_model, PDO::PARAM_STR);
-    $stmt->bindParam(':reg', $p_reg, PDO::PARAM_STR);
-    $stmt->bindParam(':km', $p_km, PDO::PARAM_INT);
-    $stmt->bindParam(':year', $p_year, PDO::PARAM_INT);
+    $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+    $stmt->bindParam(':model', $model, PDO::PARAM_STR);
+    $stmt->bindParam(':reg', $reg, PDO::PARAM_STR);
+    $stmt->bindParam(':km', $km, PDO::PARAM_INT);
+    $stmt->bindParam(':year', $year, PDO::PARAM_INT);
     return $stmt->execute();
   }
 }
